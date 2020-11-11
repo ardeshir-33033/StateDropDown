b391bfe69cf9af5bd9e28b9b1510096daf6db915
ozan <ozan3516>
2020-11-11 18:17:35 +0330

    final

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,9 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<StateModel> all = List<StateModel>();
-  String _selected;
+  String _selectedState = '1';
+  String _selectedCity;
+  var selState = 3;
 
   @override
   void initState() {
@@ -36,47 +38,110 @@ class _MyHomePageState extends State<MyHomePage> {
     String json = jsonEncode(jsonString);
     dynamic sts = jsonDecode(json);
     all = StateModel().listFromJson(sts);
-    print(all[0].cities[0].name);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          padding: EdgeInsets.all(15),
-          decoration: BoxDecoration(
-              border: Border.all(width: 1, color: Colors.grey),
-              borderRadius: BorderRadius.circular(10)),
-          child: Expanded(
-            child:  DropdownButtonHideUnderline(
-              child: ButtonTheme(
-                  alignedDropdown: true,
-                  child: DropdownButton<String>(
-                    isDense: true,
-                    hint: new Text('Select State'),
-                    value: _selected,
-                    onChanged: (String newValue) {
-                      setState(() {
-                        _selected = newValue;
-                      });
-                    },
-                    items:  jsonString.map((Map map) {
-                      return  DropdownMenuItem<String>(
-                        value: map['id'].toString(),
-                        child: Row(
-                          children: [
-                            Container(
-                              margin: EdgeInsets.only(left: 10),
-                              child: Text(map['name']),
-                            )
-                          ],
-                        ),
-                      );
-                    }).toList(),
-                  )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 20,
             ),
-          ),
+            Container(
+              width: 300,
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                  border: Border.all(width: 1, color: Colors.grey),
+                  borderRadius: BorderRadius.circular(10)),
+              child: DropdownButtonHideUnderline(
+                child: ButtonTheme(
+                    alignedDropdown: true,
+                    child: DropdownButton<String>(
+                      isDense: true,
+                      hint: Text('Select State'),
+                      value: _selectedState,
+                      onChanged: (String newValue) {
+                        setState(() {
+                          _selectedState = newValue;
+                        });
+                      },
+                      items: all.map(
+                        (e) {
+                          _selectedState = e.id;
+                          return DropdownMenuItem(
+                            value: e.id,
+                            child: Row(
+                              children: [
+                                Container(
+                                  margin: EdgeInsets.only(left: 10),
+                                  child: Text(e.name),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      ).toList(),
+                      //
+                      // items:  jsonString.map((Map map) {
+                      //   return  DropdownMenuItem<String>(
+                      //     value: map['id'].toString(),
+                      //     child: Row(
+                      //       children: [
+                      //         Container(
+                      //           margin: EdgeInsets.only(left: 10),
+                      //           child: Text(map['name']),
+                      //         )
+                      //       ],
+                      //     ),
+                      //   );
+                      // }).toList(),
+                    )),
+              ),
+            ),
+            SizedBox(height: 20,),
+            Container(
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  border: Border.all(width: 1, color: Colors.grey),
+                  borderRadius: BorderRadius.circular(10)),
+              child: DropdownButtonHideUnderline(
+                child: ButtonTheme(
+                    alignedDropdown: true,
+                    child: DropdownButton<String>(
+                      isDense: true,
+                      hint: Text('Select City'),
+                      value: _selectedCity,
+                      onChanged: (String newValue1) {
+                        setState(() {
+                          _selectedCity = newValue1;
+                        });
+                      },
+                      items: all.firstWhere((element) => element.id == _selectedState).cities.map((e) {
+                        return DropdownMenuItem(
+                          value: e.id,
+                          child: Text(e.name),
+                        );
+                      }).toList(),
+                      // items: jsonString.map((Map map) {
+                      //   return DropdownMenuItem<String>(
+                      //     value: map['id'].toString(),
+                      //     child: Row(
+                      //       children: [
+                      //         Container(
+                      //           margin: EdgeInsets.only(left: 10),
+                      //           child: Text(map['name']),
+                      //         )
+                      //       ],
+                      //     ),
+                      //   );
+                      // }).toList(),
+                    )),
+              ),
+            ),
+          ],
         ),
       ),
     );
